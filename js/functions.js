d30f56b34b737bee83732472e973d599095b40a1
jasss-u <janika.lehtola@hotmail.com>
2024-09-30 21:51:08 +0300

    master

---
+++ js/functions.js
@@ -0,0 +1,45 @@
+'use strict'
+
+let rand_num1 = 0
+let rand_num2 = 0
+let correctCount = 0
+let wrongcount = 0
+
+const getRandomIntNumberInRange = (min, max) => {
+    return Math.floor(Math.random() * max) + min;
+}
+
+const randomizeNumbers = () => {
+    rand_num1 = getRandomIntNumberInRange(1,10);
+    rand_num2 = getRandomIntNumberInRange(1,10);
+    document.querySelector ('#num1').innerHTML = rand_num1
+    document.querySelector ('#num2').innerHTML = rand_num2
+}
+
+const updateScore= () => {
+    document.querySelector('#correctcount').innerText = correctCount;
+    document.querySelector('#wrongcount').innerText = wrongcount;
+}
+
+addEventListener("DOMContentLoaded", () => {
+    randomizeNumbers()
+})
+
+document.querySelector('button').addEventListener('click', () => {
+    const answer = Number(document.querySelector('input').value)
+    const correctAnswer = rand_num1 + rand_num2;
+
+    if (answer === correctAnswer) {
+        correctCount++;
+        document.querySelector('#feedback').innerText = "Oikein!";
+    } else {
+        wrongcount++;
+        document.querySelector('#feedback').innerText = "Väärin!";
+    }
+  
+
+    randomizeNumbers()
+    document.querySelector('input').value='';
+    updateScore();
+    
+})
